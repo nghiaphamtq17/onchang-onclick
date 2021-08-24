6237cf23807693e8d065b8b1c7f71b6844ee1b7a
NghiaPhamtq17 <88359467+nghiaphamtq17@users.noreply.github.com>
2021-08-24 13:30:29 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const total = () => {
+    console.log("hello");
+  };
+  const onHandleChange = () => {
+    console.log("on handle change!!!");
+    let gt1 = document.getElementById("1");
+    let gt2 = document.getElementById("2");
+    document.querySelector("p").innerHTML = Number(gt1.value) + Number(gt2.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" name="" id="1" onChange={() => onHandleChange()} />
+        <br />
+        <input type="text" name="" id="2" onChange={() => onHandleChange()} />
+
+        <p></p>
+      </form>
+
+      <button onClick={() => total()}>tong</button>
     </div>
   );
-}
+};
 
 export default App;
